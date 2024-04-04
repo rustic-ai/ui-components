cb8acb6f3b1b4a0514cbc66f00868690333274ff
kasey <kasey@dragonscale.ai>
2024-04-04 15:10:15 -0700

    test: add tests for formatDurationTime

---
+++ cypress/unit/formatDurationTime.cy.ts
@@ -0,0 +1,26 @@
+import { formatDurationTime } from '../../src/components/helper'
+
+describe('formatDurationTime', () => {
+  it('it should show 3 digits if the duration is less than 10 seconds', () => {
+    const durationTimeInSeconds = 5
+
+    const formatted = formatDurationTime(durationTimeInSeconds)
+    expect(formatted).to.eq('0:05')
+  })
+
+  it('it should show 3 digits if the duration is between is less than 10 minutes', () => {
+    // 1 minute
+    const durationTimeInSeconds = 60
+
+    const formatted = formatDurationTime(durationTimeInSeconds)
+    expect(formatted).to.eq('1:00')
+  })
+
+  it('it should show 5 digits if the duration is between is less than 10 hours', () => {
+    // 1 hour
+    const durationTimeInSeconds = 3600
+
+    const formatted = formatDurationTime(durationTimeInSeconds)
+    expect(formatted).to.eq('1:00:00')
+  })
+})
