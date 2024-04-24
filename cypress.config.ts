1ba599dee42661228475f829b919521e65446821
kasey <kasey@dragonscale.ai>
2024-04-24 09:22:01 -0700

    chore: update cypress config for expirimental safari support

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress'
 import webpackConfig from './webpack.config'
 
 export default defineConfig({
+  experimentalWebKitSupport: true,
   chromeWebSecurity: false,
   component: {
     devServer: {
