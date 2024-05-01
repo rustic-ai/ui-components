de70068883e2668fd4d706dcc5e1b20b273e073a
Eileen Li <eileen@dragonscale.ai>
2024-05-01 14:14:27 -0700

    fix: include file names in the message

--- src/components/input/multimodal/uploader/uploader.tsx
+++ src/components/input/multimodal/uploader/uploader.tsx
@@ -8,7 +8,7 @@ import Tooltip from '@mui/material/Tooltip'
 import Typography from '@mui/material/Typography'
 import type { AxiosProgressEvent } from 'axios'
 import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { createPortal } from 'react-dom'
 import { v4 as getUUID } from 'uuid'
 
@@ -58,11 +58,13 @@ export function getFileSizeAbbrev(bytes: number): string {
 function Uploader(props: UploaderProps) {
   const [addedFiles, setAddedFiles] = useState<FileInfo[]>([])
   const [errorMessages, setErrorMessages] = useState<string[]>([])
+  const fileNamesRef = useRef<{ [key: string]: number }>({})
   const inputId = getUUID()
 
   useEffect(() => {
     setAddedFiles([])
     setErrorMessages([])
+    fileNamesRef.current = {}
   }, [props.messageId])
 
   function rejectFile(fileName: string) {
@@ -151,22 +153,41 @@ function Uploader(props: UploaderProps) {
     setAddedFiles((prev) => prev.filter((file) => file.id !== id))
   }
 
+  function getFileName(file: File) {
+    let fileName = file.name
+    const fileNameCount = fileNamesRef.current[fileName]
+
+    if (fileNameCount) {
+      const newCount = fileNameCount + 1
+      const extensionIndex = fileName.lastIndexOf('.')
+      const baseName = fileName.substring(0, extensionIndex)
+      const extension = fileName.substring(extensionIndex)
+      fileName = `${baseName}(${fileNameCount})${extension}`
+      fileNamesRef.current = { ...fileNamesRef.current, [file.name]: newCount }
+    } else {
+      fileNamesRef.current = { ...fileNamesRef.current, [file.name]: 1 }
+    }
+
+    return fileName
+  }
+
   function uploadFile(file: File) {
     const formData = new FormData()
     const controller = new AbortController()
-    formData.append('file', file)
 
+    const fileName = getFileName(file)
+    const updatedFile = new File([file], fileName, { type: file.type })
+    formData.append('file', updatedFile)
     const temporaryFileId = getUUID()
     const newAddedFile = {
-      name: file.name,
+      name: fileName,
       loadingProgress: 0,
       abortController: controller,
       id: temporaryFileId,
     }
 
     setAddedFiles((prev) => [...prev, newAddedFile])
-
-    props.handleFileCountChange(1)
+    props.onFileUpdate('add', fileName)
 
     function handleUploadProgress(progressEvent: AxiosProgressEvent) {
       const percentageConversionRate = 100
@@ -174,7 +195,7 @@ function Uploader(props: UploaderProps) {
         const loadedPercentage =
           (progressEvent.loaded / progressEvent.total) *
           percentageConversionRate
-        updateProgress(loadedPercentage, temporaryFileId)
+        updateProgress(loadedPercentage, newAddedFile.id)
       }
     }
 
@@ -185,34 +206,48 @@ function Uploader(props: UploaderProps) {
         signal: controller.signal,
       })
       .then((response) => {
-        handleSuccessfulUpload(response.data, temporaryFileId)
+        handleSuccessfulUpload(response.data, newAddedFile.id)
       })
       .catch((error) => {
-        props.handleFileCountChange(-1)
-        handleFailedUpload(file.name, temporaryFileId, error.response?.data)
+        props.onFileUpdate('remove', fileName)
+        fileNamesRef.current = {
+          ...fileNamesRef.current,
+          [fileName]: fileNamesRef.current[file.name]--,
+        }
+        handleFailedUpload(file.name, newAddedFile.id, error.response?.data)
       })
   }
 
   function handleDelete(file: FileInfo, index: number) {
     setErrorMessages([])
-    props.handleFileCountChange(-1)
+    const fileName = file.name
+    fileNamesRef.current = {
+      ...fileNamesRef.current,
+      [fileName]: fileNamesRef.current[fileName]--,
+    }
+    props.onFileUpdate('remove', fileName)
     if (file.loadingProgress === maximumLoadingProgress) {
       const deleteUrl = `${props.deleteFileEndpoint}?message-id=${props.messageId}&file-id=${file.id}`
+
       axios
         .delete(deleteUrl)
         .then(() => {
           return setAddedFiles((prev) => prev.filter((_, i) => i !== index))
         })
         .catch(() => {
-          props.handleFileCountChange(1)
+          props.onFileUpdate('add', fileName)
           return setErrorMessages((prevMessages) => [
             ...prevMessages,
-            `Failed to delete ${file.name}. Please try again.`,
+            `Failed to delete ${fileName}. Please try again.`,
           ])
         })
     } else {
       file.abortController.abort()
       setAddedFiles((prev) => prev.filter((_file, i) => i !== index))
+      fileNamesRef.current = {
+        ...fileNamesRef.current,
+        [fileName]: fileNamesRef.current[fileName]++,
+      }
     }
   }
 
