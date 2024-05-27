d899a9e4b8e91f132bfe1de8ac9a55f7dfff2fcf
kasey <kasey@dragonscale.ai>
2024-05-27 10:55:58 -0700

    fix!: update messageCanvas styling
    BREAKING CHANGE: Changes in classnames.

--- src/components/messageCanvas/messageCanvas.cy.tsx
+++ src/components/messageCanvas/messageCanvas.cy.tsx
@@ -18,20 +18,6 @@ describe('MessageCanvas', () => {
     data: { text: 'Hello World' },
   }
 
-  const testMessageUpdate = {
-    ...testMessage,
-    lastThreadMessage: {
-      id: '3',
-      timestamp: '2020-01-02T00:00:00.000Z',
-      sender: 'senderId',
-      conversationId: 'lkd9vc',
-      topicId: 'default',
-      threadId: '2',
-      format: 'text',
-      data: { text: '!' },
-    },
-  }
-
   const messageCanvas = '[data-cy=message-canvas]'
   supportedViewports.forEach((viewport) => {
     it(`renders the component on ${viewport} screen`, () => {
@@ -47,6 +33,7 @@ describe('MessageCanvas', () => {
 
       cy.contains('Hello World').should('be.visible')
       cy.contains('senderId').should('be.visible')
+      cy.contains('Jan 1, 2020').should('be.visible')
       cy.get('[data-cy="account-circle-icon"]').should('be.visible')
     })
 
@@ -78,62 +65,6 @@ describe('MessageCanvas', () => {
           expect(text).to.eq('Hello World')
         })
       })
-    })
-  })
-
-  context('Desktop', () => {
-    beforeEach(() => {
-      cy.viewport('macbook-13')
-    })
-
-    it('shows timestamp on hover', () => {
-      cy.mount(
-        <MessageCanvas message={testMessage}>
-          <p>Hello World</p>
-        </MessageCanvas>
-      )
-      cy.contains('Jan 1, 2020').should('not.be.visible')
-      cy.get(messageCanvas).realHover()
-      cy.contains('Jan 1, 2020').should('be.visible')
-    })
-
-    it('shows that it was last updated if an update is provided', () => {
-      cy.mount(
-        <MessageCanvas message={testMessageUpdate}>
-          <p>Hello World</p>
-        </MessageCanvas>
-      )
-
-      cy.get(messageCanvas).realHover()
-      cy.contains('last updated').should('be.visible')
-    })
-  })
-
-  context('Mobile', () => {
-    beforeEach(() => {
-      cy.viewport('iphone-6')
-    })
-
-    it('shows timestamp on touch', () => {
-      cy.mount(
-        <MessageCanvas message={testMessage}>
-          <p>Hello World</p>
-        </MessageCanvas>
-      )
-      cy.contains('Jan 1, 2020').should('not.be.visible')
-      cy.get(messageCanvas).realTouch()
-      cy.contains('Jan 1, 2020').should('be.visible')
-    })
-
-    it('shows that it was last updated if an update is provided', () => {
-      cy.mount(
-        <MessageCanvas message={testMessageUpdate}>
-          <p>Hello World</p>
-        </MessageCanvas>
-      )
-
-      cy.get(messageCanvas).realTouch()
-      cy.contains('last updated').should('be.visible')
     })
   })
 })
