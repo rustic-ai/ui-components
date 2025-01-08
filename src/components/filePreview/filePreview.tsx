77195045656941e9e0dc3d8aba97a0f54b0a7634
Eileen Li <eileen@dragonscale.ai>
2025-01-08 14:23:12 -0800

    fix: add space between download icon and file name

--- src/components/filePreview/filePreview.tsx
+++ src/components/filePreview/filePreview.tsx
@@ -72,7 +72,11 @@ export default function FilePreview(
         sx={{ boxShadow: theme.shadows[1] }}
         onClick={handleFileClick}
       >
-        <Typography variant="subtitle2" data-cy="file-name">
+        <Typography
+          variant="subtitle2"
+          data-cy="file-name"
+          className="rustic-file-preview-name"
+        >
           {fileName}
         </Typography>
         <div onClick={(e) => e.stopPropagation()}>{props.children}</div>
